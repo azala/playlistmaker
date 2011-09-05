97f65835bb5c01432ba6060f85b1404098fcf468
mdsa <mdsa@funzio.com>
2011-09-04 23:34:01 -0700

    rename

---
+++ adhoc_rename.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import plvars as plv, os
+
+#os.system('./rename.py "'+plv.NEWESTPATH+'" 1110')
+#os.system('./rename.py "'+plv.REALLYNEWESTPATH+'" 1110')
+#os.system('./rename.py "'+plv.DESKTOP+'" 1010')
+
+os.system('./rename.py "'+plv.LOCALSONGSPATH+'" 1110')
