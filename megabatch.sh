77563fe34c613709c340f055cf64170ea4fb010c
mdsa <mdsa@funzio.com>
2012-10-27 22:08:44 -0700

    fix up preprocess code for mac

--- megabatch.sh
+++ megabatch.sh
@@ -2,4 +2,5 @@ cd ~/pystuff/playlistmaker
 ./adhoc_rename.py
 ./movestuff.py $@
 ./dirfill.py -a
-echo /new;// > $PLAYLISTMAKER_DIR/preprocess.txt
+echo "/new;//" > $PLAYLISTMAKER_DIR/data/preprocess.txt
+./playlistmaker.py
